8bec84fe87953bdd83ceebb1756016ef545e6a96
Eduardo Tosta <eduardo_tosta@outlook.com>
2025-04-25 09:39:58 -0300

    Ajustes no menu e troca de cores

--- src/conta_bancaria/Menu.java
+++ src/conta_bancaria/Menu.java
@@ -7,23 +7,25 @@ public class Menu {
 	public static void main(String[] args) {
 		
 		
-		 System.out.println(Cores.ANSI_PURPLE_BACKGROUND + Cores.TEXT_GREEN_BOLD);
-		 System.out.println("╔════════════════════════════════════════╗");
-	     System.out.println("║          BANCO DO BRAZIL COM Z         ║");
-	     System.out.println("╠════════════════════════════════════════╣");
-	     System.out.println("║  1 - Criar conta                       ║");
-	     System.out.println("║  2 - Listar todas as contas            ║");
-	     System.out.println("║  3 - Buscar Conta por Número           ║");
-	     System.out.println("║  4 - Atualizar Dados da Conta          ║");
-	     System.out.println("║  5 - Apagar conta                      ║");
-	     System.out.println("║  6 - Sacar                             ║");
-	     System.out.println("║  7 - Depositar                         ║");
-	     System.out.println("║  8 - Transferir valor entre contas     ║");
-	     System.out.println("║  9 - Sair                              ║");
-	     System.out.println("╚════════════════════════════════════════╝");
-	     System.out.println("==========================================");
-	     System.out.println("Digite a opção desejada:                  ");
-	     System.out.println("==========================================");
+		 System.out.println(Cores.ANSI_RED_BACKGROUND + Cores.TEXT_WHITE_BOLD);
+		 System.out.println(" ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ");
+		 System.out.println(" ■ ╔════════════════════════════════════════╗  ■ ");
+	     System.out.println(" ■ ║          BANCO DO BRAZIL COM Z         ║  ■ ");
+	     System.out.println(" ■ ╠════════════════════════════════════════╣  ■ ");
+	     System.out.println(" ■ ║  1 - Criar conta                       ║  ■ ");
+	     System.out.println(" ■ ║  2 - Listar todas as contas            ║  ■ ");
+	     System.out.println(" ■ ║  3 - Buscar Conta por Número           ║  ■ ");
+	     System.out.println(" ■ ║  4 - Atualizar Dados da Conta          ║  ■ ");
+	     System.out.println(" ■ ║  5 - Apagar conta                      ║  ■ ");
+	     System.out.println(" ■ ║  6 - Sacar                             ║  ■ ");
+	     System.out.println(" ■ ║  7 - Depositar                         ║  ■ ");
+	     System.out.println(" ■ ║  8 - Transferir valor entre contas     ║  ■ ");
+	     System.out.println(" ■ ║  9 - Sair                              ║  ■ ");
+	     System.out.println(" ■ ╚════════════════════════════════════════╝  ■ ");
+	     System.out.println(" ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ");
+	     System.out.println("=================================================");
+	     System.out.println(" Digite a opção desejada:                        ");
+	     System.out.println("=================================================");
 	     
 	     
 	     
