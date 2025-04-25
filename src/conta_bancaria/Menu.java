cf17ec5527f3c22a3e845241260941fc672806c2
Eduardo Tosta <eduardo_tosta@outlook.com>
2025-04-25 11:06:16 -0300

    Criação da classe Conta e testes na classe Menu

--- src/conta_bancaria/Menu.java
+++ src/conta_bancaria/Menu.java
@@ -1,33 +1,46 @@
 package conta_bancaria;
 
+import conta_bancaria.model.Conta;
 import conta_bancaria.util.Cores;
 
 public class Menu {
-
 	public static void main(String[] args) {
 		
+		//Instanciar um objeto da classe conta
+		Conta c1 = new Conta(01, 123, 1, "Eduardo", 500000000);
 		
-		 System.out.println(Cores.ANSI_RED_BACKGROUND + Cores.TEXT_WHITE_BOLD);
-		 System.out.println(" ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ");
-		 System.out.println(" ■ ╔════════════════════════════════════════╗  ■ ");
-	     System.out.println(" ■ ║          BANCO DO BRAZIL COM Z         ║  ■ ");
-	     System.out.println(" ■ ╠════════════════════════════════════════╣  ■ ");
-	     System.out.println(" ■ ║  1 - Criar conta                       ║  ■ ");
-	     System.out.println(" ■ ║  2 - Listar todas as contas            ║  ■ ");
-	     System.out.println(" ■ ║  3 - Buscar Conta por Número           ║  ■ ");
-	     System.out.println(" ■ ║  4 - Atualizar Dados da Conta          ║  ■ ");
-	     System.out.println(" ■ ║  5 - Apagar conta                      ║  ■ ");
-	     System.out.println(" ■ ║  6 - Sacar                             ║  ■ ");
-	     System.out.println(" ■ ║  7 - Depositar                         ║  ■ ");
-	     System.out.println(" ■ ║  8 - Transferir valor entre contas     ║  ■ ");
-	     System.out.println(" ■ ║  9 - Sair                              ║  ■ ");
-	     System.out.println(" ■ ╚════════════════════════════════════════╝  ■ ");
-	     System.out.println(" ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ");
-	     System.out.println("=================================================");
-	     System.out.println(" Digite a opção desejada:                        ");
-	     System.out.println("=================================================");
-	     
-	     
+		c1.sacar(500);
+		c1.vizualizar(); 
+		
+		c1.depositar(200);
+		c1.vizualizar();
+		
+		//Alterar proprietário
+		c1.setTitular("Eduardo Tosta");
+		c1.vizualizar();
+		
+		
+		 System.out.println(Cores.ANSI_BLACK_BACKGROUND + Cores.TEXT_GREEN_BOLD);
+		 System.out.println(" ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■■ ");
+		 System.out.println(" ■  ╔════════════════════════════════════════╗  ■ ");
+		 System.out.println(" ■  ║                                        ║  ■ ");
+	     System.out.println(" ■  ║          BANCO DO BRAZIL COM Z         ║  ■ ");
+	     System.out.println(" ■  ║                                        ║  ■ ");
+	     System.out.println(" ■  ╠════════════════════════════════════════╣  ■ ");
+	     System.out.println(" ■  ║  1 - Criar conta                       ║  ■ ");
+	     System.out.println(" ■  ║  2 - Listar todas as contas            ║  ■ ");
+	     System.out.println(" ■  ║  3 - Buscar Conta por Número           ║  ■ ");
+	     System.out.println(" ■  ║  4 - Atualizar Dados da Conta          ║  ■ ");
+	     System.out.println(" ■  ║  5 - Apagar conta                      ║  ■ ");
+	     System.out.println(" ■  ║  6 - Sacar                             ║  ■ ");
+	     System.out.println(" ■  ║  7 - Depositar                         ║  ■ ");
+	     System.out.println(" ■  ║  8 - Transferir valor entre contas     ║  ■ ");
+	     System.out.println(" ■  ║  9 - Sair                              ║  ■ ");
+	     System.out.println(" ■  ╚════════════════════════════════════════╝  ■ ");
+	     System.out.println(" ■■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ■ ");
+	     System.out.println("==================================================");
+	     System.out.println(" Digite a opção desejada:                         ");
+	     System.out.println("==================================================");
 	     
 	     
 	}
---
+++ src/conta_bancaria/model/Conta.java
@@ -0,0 +1,109 @@
+package conta_bancaria.model;
+
+import java.text.NumberFormat;
+
+public class Conta {
+	
+	//Atributos da Classe
+	private int numero;
+	private int agencia;
+	private int tipo;
+	private String titular;
+	private double saldo;
+	
+	
+	//Metodo Construtor
+	public Conta(int numero, int agencia, int tipo, String titular, double saldo) {
+		this.numero = numero;
+		this.agencia = agencia;
+		this.tipo = tipo;
+		this.titular = titular;
+		this.saldo = saldo;
+	}
+	
+	
+	//Metodos Bancários
+	public boolean sacar(float valor) {
+		if(this.saldo < valor) {
+			System.out.println("\nSaldo insuficiente");
+			return false;
+		}
+		
+		this.saldo -= valor;
+		return true;
+	}
+	
+	public void depositar(float valor) {
+		if(valor <=0)
+		System.out.println("Valor Inválido!");	
+			
+		this.saldo += valor;
+	}
+	
+	
+	//Metodos de impressão
+	public void vizualizar() {
+		NumberFormat nfMoeda = NumberFormat.getCurrencyInstance();
+		
+		String tipo = "";
+		
+		switch(this.tipo) {
+			case 1 -> tipo = "Conta Corrente";
+			case 2 -> tipo = "Conta Poupança";
+			default -> tipo = "Inválido";
+		}
+		System.out.println("**************************************************");
+		System.out.println("                 DADOS DA CONTA                   ");
+		System.out.println("**************************************************");
+		System.out.println("Número da Conta:      " + this.numero);
+		System.out.println("Número da Agência:    " + this.agencia);
+		System.out.println("Tipo da conta:        " + tipo);
+		System.out.println("Titular da conta:     " + this.titular);
+		System.out.println("Saldo da conta:       " + nfMoeda.format(this.saldo));
+	}
+	
+	
+	//Getters e Setters
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int conta) {
+		this.numero = conta;
+	}
+
+	public int getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+
+	public int getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getTitular() {
+		return titular;
+	}
+
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(float saldo) {
+		this.saldo = saldo;
+	}
+	
+	
+	
+}
